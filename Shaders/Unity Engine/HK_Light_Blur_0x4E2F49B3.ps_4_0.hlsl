8d3166811b0c1184e871936c099966ecf0919ba6
PixelKiri <alexchirila87@gmail.com>
2025-11-10 07:54:02 +0200

    Better logic that adjusts the texcoord in order to fix the offset

--- Shaders/Unity Engine/HK_Light_Blur_0x4E2F49B3.ps_4_0.hlsl
+++ Shaders/Unity Engine/HK_Light_Blur_0x4E2F49B3.ps_4_0.hlsl
@@ -1,5 +1,3 @@
-#include "../shared.h"
-
 // ---- Created with 3Dmigoto v1.3.16 on Thu Sep  4 10:46:26 2025
 Texture2D<float4> t0 : register(t0);
 
@@ -20,19 +18,28 @@ void main(
   uint4 bitmask, uiDest;
   float4 fDest;
 
-  r0.xz = v1.xx;
-  float offsetCorrection = -0.307691946;
-  float4 offsetCorrectionFloat4 = cb0[2].y * float4(offsetCorrection, offsetCorrection, offsetCorrection, offsetCorrection);
-  r1.xyzw = cb0[2].yyyy * float4(1.38461494, 3.23076892, -0.615384996, -2.76923108) + v1.yyyy + offsetCorrectionFloat4;
+  // https://www.rastergrid.com/blog/2010/09/efficient-gaussian-blur-with-linear-sampling/
+  // GaussianOffset(3, 4, 220, 066);  // 3.230769230769231
+  // GaussianOffset(1, 2, 792, 495);  // 1.3846153846153846
+  // GaussianOffset(2, 4, 792, 495);  // 2.769230769230769
+  // GaussianOffset(1, 0, 792, 495);  // 0.615384996
+  // float weight1 = 0.316260993;
+  // float weight2 = 0.0702700019;
+  // float offset = -(weight1 * (1.38461494f + -0.615384996) + (weight2 * (3.23076892f + -2.76923108))) / 2.0f;
+  float offset = -0.137854845;
+  float2 adjustedv1 = v1 + cb0[2].xy * offset;
+
+  r0.xz = adjustedv1.xx;
+  r1.xyzw = cb0[2].yyyy * float4(1.38461494, 3.23076892, -0.615384996, -2.76923108) + adjustedv1.yyyy;
   r0.yw = r1.xz;
   r2.xyzw = t0.Sample(s0_s, r0.xy).xyzw;
   r0.xyzw = t0.Sample(s0_s, r0.zw).xyzw;
   r0.xyz = r2.xyz + r0.xyz;
   r0.xyz = float3(0.316260993, 0.316260993, 0.316260993) * r0.xyz;
-  r2.xyzw = t0.Sample(s0_s, v1.xy).xyzw;
+  r2.xyzw = t0.Sample(s0_s, adjustedv1.xy).xyzw;
   float4 r3 = r2;
   r0.xyz = r2.xyz * float3(0.227026999, 0.227026999, 0.227026999) + r0.xyz;
-  r1.xz = v1.xx;
+  r1.xz = adjustedv1.xx;
   r2.xyzw = t0.Sample(s0_s, r1.xy).xyzw;
   r1.xyzw = t0.Sample(s0_s, r1.zw).xyzw;
   r1.xyz = r2.xyz + r1.xyz;
--- Shaders/Unity Engine/HK_Light_Blur_0xC7659A76.ps_4_0.hlsl
+++ Shaders/Unity Engine/HK_Light_Blur_0xC7659A76.ps_4_0.hlsl
@@ -1,4 +1,3 @@
-#include "../shared.h"
 // ---- Created with 3Dmigoto v1.3.16 on Thu Sep  4 10:46:27 2025
 Texture2D<float4> t0 : register(t0);
 
@@ -19,19 +18,28 @@ void main(
   uint4 bitmask, uiDest;
   float4 fDest;
 
-  float offsetCorrection = -0.307691946;
-  float4 offsetCorrectionFloat4 = cb0[2].x * float4(offsetCorrection, offsetCorrection, offsetCorrection, offsetCorrection);
-  r0.xyzw = cb0[2].xxxx * float4(3.23076892, 1.38461494, -2.76923108, -0.615384996) + v1.xxxx + offsetCorrectionFloat4;
+  // https://www.rastergrid.com/blog/2010/09/efficient-gaussian-blur-with-linear-sampling/
+  // GaussianOffset(3, 4, 220, 066);  // 3.230769230769231
+  // GaussianOffset(1, 2, 792, 495);  // 1.3846153846153846
+  // GaussianOffset(2, 4, 792, 495);  // 2.769230769230769
+  // GaussianOffset(1, 0, 792, 495);  // 0.615384996
+  // float weight1 = 0.316260993;
+  // float weight2 = 0.0702700019;
+  // float offset = -(weight1 * (1.38461494f + -0.615384996) + (weight2 * (3.23076892f + -2.76923108))) / 2.0f;
+  float offset = -0.137854845;
+  float2 adjustedv1 = v1 + cb0[2].xy * offset;
+
+  r0.xyzw = cb0[2].xxxx * float4(3.23076892, 1.38461494, -2.76923108, -0.615384996) + adjustedv1.xxxx;
   r1.xz = r0.yw;
-  r1.yw = v1.yy;
+  r1.yw = adjustedv1.yy;
   r2.xyzw = t0.Sample(s0_s, r1.xy).xyzw;
   r1.xyzw = t0.Sample(s0_s, r1.zw).xyzw;
   r1.xyz = r2.xyz + r1.xyz;
   r1.xyz = float3(0.316260993, 0.316260993, 0.316260993) * r1.xyz;
-  r2.xyzw = t0.Sample(s0_s, v1.xy).xyzw;
+  r2.xyzw = t0.Sample(s0_s, adjustedv1.xy).xyzw;
   float4 r3 = r2;
   r1.xyz = r2.xyz * float3(0.227026999, 0.227026999, 0.227026999) + r1.xyz;
-  r0.yw = v1.yy;
+  r0.yw = adjustedv1.yy;
   r2.xyzw = t0.Sample(s0_s, r0.xy).xyzw;
   r0.xyzw = t0.Sample(s0_s, r0.zw).xyzw;
   r0.xyz = r2.xyz + r0.xyz;
